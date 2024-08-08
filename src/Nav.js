509fdd157d161efdcde1bbb9637d7e53a74559f8
Sachin Pathak <103988614+sachinpathak123@users.noreply.github.com>
2024-08-08 14:19:38 +0530

    Add files via upload
    uploaded files

---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './App.css';
+import { FaBars } from 'react-icons/fa';
+function Nav() {
+    const [menu,setmenu]=useState(false);
+  return (
+    <div className='navbar'>
+        <div className="elements">
+      <div className="navleft">
+      <FaBars size={27} color="black"/>
+      </div>
+      <div className="logo">
+        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20210224040124/JSBinCollaborativeJavaScriptDebugging6-300x160.png" alt="gfg logo" className='gfglogo' />
+      </div>
+      <div className="navright">
+        Hello
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
